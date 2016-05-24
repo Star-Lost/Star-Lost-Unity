6600ce6e41c9c834180bd963dead241947197694
Ronny Holm <RonnyHolm@users.noreply.github.com>
2016-05-24 15:06:35 +0200

    8 directional movement example

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float WalkSpeed = 2.0f;
+
+	private Animator animator;
+
+	void Start() {
+		animator = GetComponent<Animator> ();
+	}
+	
+	void Update () {
+		var horizontal = Input.GetAxisRaw ("Horizontal");
+		var vertical = Input.GetAxisRaw ("Vertical");
+		var isWalking = horizontal != 0 || vertical != 0;
+
+		animator.SetBool ("IsWalking", isWalking);
+		if (isWalking) {
+			animator.SetFloat ("XVelocity", horizontal);
+			animator.SetFloat ("YVelocity", vertical);
+		}
+
+		transform.Translate (new Vector2 (horizontal, vertical) * WalkSpeed * Time.deltaTime);
+	}
+
+}
