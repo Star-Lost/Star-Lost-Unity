41f04daf0af485d1f5b91b0b9793f768c57ac5f2
Ronny Holm <RonnyHolm@users.noreply.github.com>
2016-05-25 17:13:37 +0200

    Messing with shaders

---
+++ Assets/ShaderTest/ImageEffect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ImageEffect : MonoBehaviour {
+
+	public Material ImageEffectMaterial;
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		Graphics.Blit (src, dest, ImageEffectMaterial);
+	}
+
+}
---
+++ Assets/ShaderTest/SpinCube.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinCube : MonoBehaviour {
+	void Update () {
+		transform.RotateAround (Vector3.up, .3f * Time.deltaTime);
+	}
+}
